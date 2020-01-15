7a5ebb670f20748296978726344367e689ecb41d
AnonymousNoob <theincredible13@gmail.com>
2020-01-16 01:00:51 +0530

    Add sample application

---
+++ istio/setup-sample-webapp.sh
@@ -0,0 +1,3 @@
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+sleep 5
+kubectl exec -it $(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl productpage:9080/productpage | grep -o "<title>.*</title>"
